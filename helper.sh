7608a690a0df690a12dc451bb440d7b573f8537d
Rahul Tiwari <jprrahultiwari@gmail.com>
2021-11-15 11:47:31 +0530

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -20,30 +20,6 @@ contents="$(jq -n \
    <<<'{}'
 )"
 rm app/flavours/flavourfiles.json
-rm marketPlace/flavours/flavourfiles.json
 
 echo "$contents"
 echo "$contents"| jq -n '.flavours |= [inputs]'> app/flavours/flavourfiles.json
-echo "$contents"| jq -n '.flavours |= [inputs]'> marketPlace/flavours/flavourfiles.json
-
-# Asset generation
-# Prerequistes for the below is a gm binary.
-# Run `brew install graphicsmagick` on Mac
-# Run `sudo apt-get install graphicsmagick` on Linux
-APP_DIR=app/src/$3
-rm -rf $APP_DIR/res
-mkdir -p $APP_DIR/res
-mkdir -p $APP_DIR/res/mipmap-xxxhdpi
-mkdir -p $APP_DIR/res/mipmap-xxhdpi
-mkdir -p $APP_DIR/res/mipmap-xhdpi
-mkdir -p $APP_DIR/res/mipmap-hdpi
-mkdir -p $APP_DIR/res/mipmap-mdpi
-curl -sSf -u "admin:Winuall@123" "$6" -o $APP_DIR/ic_launcher.png
-convert $APP_DIR/ic_launcher.png -resize 144x144 $APP_DIR/res/mipmap-xxhdpi/ic_launcher.png
-convert $APP_DIR/ic_launcher.png -resize 96x96 $APP_DIR/res/mipmap-xhdpi/ic_launcher.png
-convert $APP_DIR/ic_launcher.png -resize 72x72 $APP_DIR/res/mipmap-hdpi/ic_launcher.png
-convert $APP_DIR/ic_launcher.png -resize 48x48 $APP_DIR/res/mipmap-mdpi/ic_launcher.png
-convert $APP_DIR/ic_launcher.png -resize 192x192 $APP_DIR/res/mipmap-xxxhdpi/ic_launcher.png
-
-# Format google-services.json
-jq --arg var "$2" '.client[0].client_info.android_client_info.package_name = $var' app/google-services.json|sponge app/google-services.json
